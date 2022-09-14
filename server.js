c5afdfa5989305593783a55abf9dd60667e172bd
IBassaoud <ismail.bassaoud@gmail.com>
2022-09-14 09:15:04 +0000

    Init

--- server.js
+++ server.js
@@ -4,11 +4,29 @@ const express = require('express');
 const PORT = 80;
 const HOST = 'localhost';
 
-const dotenv = require('dotenv').config('./config');
+const dotenv = require('dotenv').config();
+console.log(process.env.MONGO_USER);
 
+// DB
 const MongoClient = require('mongodb').MongoClient
 const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
 
+// CRUD OPERATIONS
+// Add one document
+MongoClient.connect(url, function(err, database) {
+    if (err) throw err;
+    var dbo = database.db("db_parking");
+    var myobj = { 
+        name: "Parking 1", 
+        type: "AIRPORT",
+        city: "London",
+    };
+    dbo.collection("parking").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      database.close();
+    });
+  });
 
 // App
 const app = express();
